301727e8fdf78659bcb851c869922104fc9a44ac
SaiChandra2896 <saichandra2896@gmail.com>
2020-03-03 13:56:19 +0530

    add binary search and complete searching algorithms

---
+++ SEARCHING_ALGORITHMS/BinarySearch/BinarySearch.js
@@ -0,0 +1,34 @@
+//Pseudocode in binary search.txt
+// Original Solution
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while (arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if (arr[middle] === elem) {
+        return middle;
+    }
+    return -1;
+}
+
+// Refactored Version ES6
+function binarySearchRefactored(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while (arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
+
+binarySearchRefactored([2, 5, 6, 9, 13, 15, 28, 30], 103)
