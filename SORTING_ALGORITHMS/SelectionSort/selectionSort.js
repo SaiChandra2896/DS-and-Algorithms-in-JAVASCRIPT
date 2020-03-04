21b80fdf6083a0713d616b783351c2a19d676f26
SaiChandra2896 <saichandra2896@gmail.com>
2020-03-04 15:20:26 +0530

    add selection sort

---
+++ SORTING_ALGORITHMS/SelectionSort/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr, 'old array')
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) min = j;
+        }
+        if (i !== min) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+            console.log(arr, 'array')
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort([10, 9, 3, 4]));
